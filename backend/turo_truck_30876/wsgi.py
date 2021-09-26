beb90203c8a4dff0b0075af992820d1e8ba21c92
Crowdbotics <team@crowdbotics.com>
2021-09-26 11:01:34 +0000

    Initial commit.

---
+++ backend/turo_truck_30876/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for turo_truck_30876 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turo_truck_30876.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "turo_truck_30876",
+  url: "https://turo_truck_30876.botics.co",
+  api: "https://turo_truck_30876.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
